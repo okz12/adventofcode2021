2f71f8b063d9dfb82fad51023e90750624f85475
Osman <5850274+okz12@users.noreply.github.com>
2021-12-04 08:02:02 -0500

    Day 1 puzzle

---
+++ 01/01.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+def count_depth_increase(depths: List[int]) -> int:
+    return sum(b > a for a, b in zip(depths[:-1], depths[1:]))
+
+
+def sliding_window_3(depths: List[int]) -> List[int]:
+    return [depths[n] + depths[n + 1] + depths[n + 2] for n in range(len(depths) - 2)]
+
+
+if __name__ == "__main__":
+    testcase = """\
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263\
+"""
+    with open("input.txt", "r") as f:
+        data = f.read()
+
+    testcase_nums = [int(x) for x in testcase.split()]
+    data_nums = [int(x) for x in data.split()]
+    print(count_depth_increase(testcase_nums))
+    print(count_depth_increase(data_nums))
+    print(count_depth_increase(sliding_window_3(testcase_nums)))
+    print(count_depth_increase(sliding_window_3(data_nums)))
---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2020
+
+### Christmas flavoured coding puzzles
+
+See challenge descriptions here: https://adventofcode.com/2021
+
+Code in Python 3.9
